cdd8b6d10394affa8418b299f8a539b6305a9292
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-07-16 21:41:43 +0300

    fix dependence

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,14 +1,16 @@
-const doteenv = require("dotenv");
-doteenv.config({ path: "./.env.deploy" });
+const doteenv = require('dotenv');
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY } =
-  process.env;
+doteenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
+} = process.env;
 
 module.exports = {
   apps: [
     {
-      name: "mesto",
-      script: ".dist/app.js",
+      name: 'mesto',
+      script: '.dist/app.js',
     },
   ],
 
@@ -19,8 +21,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      "post-deploy": "cd backend && pwd&& npm ci && npm run build && pm2 startOrRestart ecosystem.config.js env production",
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd&& npm ci && npm run build && pm2 startOrRestart ecosystem.config.js env production',
     },
   },
 };
