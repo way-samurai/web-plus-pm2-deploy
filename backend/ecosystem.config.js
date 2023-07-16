ca4bbae207a6419165f0745f18e0488b8868db0c
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-07-16 23:06:27 +0300

    fix ecosystem.config.js исправил скрипт post-deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd&& npm ci && npm run build && pm2 startOrRestart ecosystem.config.js env production',
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
