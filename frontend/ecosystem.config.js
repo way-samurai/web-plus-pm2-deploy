2eedf03de7101b6ca400f229b9a510fe0cbc75dd
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-07-17 03:23:36 +0300

    ПДобавил конфик ecosystem для фронтенда

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,27 @@
+const doteenv = require('dotenv');
+
+doteenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto',
+      script: './dist/app.js',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
+    },
+  },
+};
